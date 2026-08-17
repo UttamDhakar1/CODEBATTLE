58ca84ec2d851bf566ff4ecb102b2a9ce41e0496
Uttam Dhakar <uttamdhakar1234@gmail.com>
2026-08-18 01:22:37 +0530

    Connect MySQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,33 @@ const express = require("express");
 const cors = require("cors");
 // CORS -> CORS is a browswer security mechanism that controls
 //  which frontend application is allowed to send backen API
+
 const app = express();
-
-const PORT = 8000;
-
 app.use(cors());
 app.use(express.json());
+const sequelize = require("./config/database.js");
+
+const PORT =  process.env.PORT || 8000;
+
 app.get("/", (req, res) => {
     res.json({
         message: "CODEBATTLE Backend is Running !!",
     });
     // res.end("CodeBattle is running");
 });
-app.listen(PORT, ()=>{
-    console.log(`CODEBATTLE is running on port ${PORT}`);
-});
+
+const startServer = async() => {
+    try{
+        await sequelize.authenticate();
+        console.log("MySQL database connected");
+        app.listen(PORT, () =>{
+            console.log(`CODEBATTLE is running on port ${PORT}`);
+        });
+    }
+    catch(error){
+        console.log("Unable to connect to MySQL");
+        console.error(error.message);
+    }
+};
+
+startServer();
